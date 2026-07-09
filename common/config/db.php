29831254711411913564e4dee2d790921dbc8227
Cocfay <cocfay@weclickdigital.com>
2026-07-09 16:04:47 -0600

    Camdios

--- common/config/db.php
+++ common/config/db.php
@@ -1,9 +1,9 @@
 <?php
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=devblogbrickcly',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=db-mysql-blog-produccion-do-user-36689743-0.h.db.ondigitalocean.com:25060;dbname=PROBlogBrickly',
+    'username' => 'PROBlogBrickly',
+    'password' => '(F$4nzD34min',
     'charset' => 'utf8mb4',
     ];
 
